23b65624f4e3c96f11c7c180c0456d07363c8157
laurivasyyy <90681617+laurivasyyy@users.noreply.github.com>
2023-10-16 01:28:36 +0700

    Initial Commit

---
+++ Dashboard/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Dashboard.Core;
+using System;
+
+namespace Dashboard.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DiscoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set 
+            { 
+                _currentView = value;
+                onPropertyChanged();
+            }
+
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DiscoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DiscoveryVM;
+            });
+        }
+    }
+}
